1cc33640e2d8532bffe58a4b7177243f88065cf4
EC2 Default User <ec2-user@ip-172-31-86-69.ec2.internal>
2019-06-28 15:20:17 +0000

    mudanças no BD

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from wtforms.validators import DataRequired, NumberRange
 from wtforms.widgets.html5 import NumberInput
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
-
+#from enum import Enum
 # https://devcenter.heroku.com/articles/heroku-postgresql
 # https://devcenter.heroku.com/articles/heroku-cli
 
@@ -26,6 +26,11 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 migrate = Migrate(app, db)
 
+def cadastraAlunos(listaDeNomes, serie):
+    for nomes in listaDeNomes.split():
+        db.session.add(Alunos(aluno=nomes, serie=serie))
+        #db.session.commit()
+
 class Cadastro(FlaskForm):
     serie = StringField('Série', validators=[DataRequired()])
     turma = StringField('Participantes', validators=[DataRequired()])
@@ -33,49 +38,53 @@ class Cadastro(FlaskForm):
     
 class Criar_votacao(FlaskForm):
     titulo = StringField('Título', validators=[DataRequired()])
-    opcoes = StringField('Opção 1')
-    opcoes1 = StringField('Opção 2')
+    opcoes = StringField('Opção 1', validators=[DataRequired()])
+    opcoes1 = StringField('Opção 2', validators=[DataRequired()])
     opcoes2 = StringField('Opção 3')
     opcoes3 = StringField('Opção 4')
     opcoes4 = StringField('Opção 5')
     
     submit5 = SubmitField('Criar')
-    
+
+class Alunos(db.Model):
+    __tablename__="Alunos"
+    aluno = db.Column(db.String(64), primary_key=True)
+    serie = db.Column(db.String(30), db.ForeignKey('Sala.serie'))
+
 class Votacao(db.Model):
     __tablename__="Votacao"
-    id = db.Column(db.String(5), primary_key=True, index=True)
-    
-    titulo= db.Column(db.String (30))
+
+    titulo= db.Column(db.String (30), primary_key=True)
     opcao1= db.Column(db.String (20))
     opcao2= db.Column(db.String (20))
     opcao3= db.Column(db.String (20))
     opcao4= db.Column(db.String (20))
     opcao5= db.Column(db.String (20))
     
-    sala = db.relationship('Sala', backref='role')
+    serie = db.Column(db.String(30), db.ForeignKey('Sala.serie'))
     
     def __repr__ (self):
-        return '<id: %r>' % self.id
+        return '<titulo: %r>' % self.titulo
 
 class Sala(db.Model):
     __tablename__="Sala"
-    id = db.Column(db.String(5), primary_key=True, index=True)
-    serie= db.Column(db.String (30))
-    turma= db.Column(db.Text (200))
+    serie= db.Column(db.String (30), primary_key=True, index=True)
     
-    vot_id = db.Column(db.String(5), db.ForeignKey('Votacao.id'))
+    votacoes = db.relationship('Votacao', backref='Sala')
+    alunos = db.relationship('Alunos', backref='Sala')
     
     def __repr__ (self):
-        return '<id: %r>' % self.id
+        return '<serie: %r;'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = Cadastro()
-    
+    votacao = criarVotacao()
     if form.validate_on_submit():
-        db.session.add(Sala(id=str(len(Sala.query.all())+1), serie=form.serie.data, turma=form.turma.data))
-        db.session.commit()
-        return redirect(url_for('index'))
+        db.session.add(Sala(serie=form.serie.data)) #cadastra sala
+        cadastraAlunos(form.turma.data, serie=form.serie.data) # cadastra alunos
+        db.session.commit()# slava aletrações
+        return redirect(url_for('criarVotacao', form=votacao))
     
     return render_template('index.html', form=form)
     
@@ -83,6 +92,10 @@ def index():
 def criarVotacao():
     form = Criar_votacao()
     
+    if form.validate_on_submit():
+        if form.titulo.data != None or form.opcoes.data != None or form.opcoes1.data != None or form.opcoes2.data != None or form.opcoes3.data != None or form.opcoes4.data != None:
+            db.session.add(Votacao(titulo=form.titulo.data, opcao1=form.opcoes.data, opcao2=form.opcoes1.data , opcao3=form.opcoes2, opcao4=form.opcoes3.data, opcao5=form.opcoes4.data))
+            db.session.commit
     return render_template('criarVotacao.html', form=form)
     
 @app.route('/votacao-criadas')
